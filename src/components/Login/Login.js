f730bd20891b75b1caf7c6bb11b9e3fd15db28c6
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-02-06 20:37:22 +0530

    Refactoring input component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,33 +1,35 @@
-import React, { useState,  useReducer } from 'react';
-
+import React, { useState, useReducer } from 'react';
+import { useContext } from 'react';
+import AuthContext from '../../Store/auth-context';
 import Card from '../UI/Card/Card';
 import classes from './Login.module.css';
 import Button from '../UI/Button/Button';
+import Input from '../UI/Input/Input';
 
 const emailReducer = (state, action) => {
-  if(action.type === 'User_Input'){
+  if (action.type === 'User_Input') {
     return { value: action.val, isValid: action.val.includes('@') };
   }
-  if(action.type === 'Input_Blur'){
+  if (action.type === 'Input_Blur') {
     return { value: state.value, isValid: state.value.includes('@') };
   }
   return { value: '', isValid: false };
 }
 const passwordReducer = (state, action) => {
-  if(action.type === 'User_Input'){
+  if (action.type === 'User_Input') {
     return { value: action.val, isValid: action.val.trim().length > 6 };
   }
-  if(action.type === 'Input_Blur'){
+  if (action.type === 'Input_Blur') {
     return { value: state.value, isValid: state.value.trim().length > 6 };
   }
   return { value: '', isValid: false };
 }
 const collegeNamelReducer = (state, action) => {
-  if(action.type === 'User_Input'){
+  if (action.type === 'User_Input') {
     return { value: action.val, isValid: action.val.trim().length > 0 };
   }
-  if(action.type === 'Input_Blur'){
-    return { value: state.value, isValid: state.value.trim().length > 0};
+  if (action.type === 'Input_Blur') {
+    return { value: state.value, isValid: state.value.trim().length > 0 };
   }
   return { value: '', isValid: false };
 }
@@ -66,7 +68,7 @@ const Login = (props) => {
   // }, [enteredEmail, enteredPassword, enteredCollegeName])
 
   const emailChangeHandler = (event) => {
-    dispatchEmail({type:'User_Input', val:event.target.value})
+    dispatchEmail({ type: 'User_Input', val: event.target.value })
     setFormIsValid(
       emailState.isValid && passwordState.isValid && collegeNameState.isValid
     );
@@ -74,7 +76,7 @@ const Login = (props) => {
   };
 
   const passwordChangeHandler = (event) => {
-    dispatchPassword({type:'User_Input', val:event.target.value})
+    dispatchPassword({ type: 'User_Input', val: event.target.value })
     setFormIsValid(
       emailState.isValid && passwordState.isValid && collegeNameState.isValid
     );
@@ -82,70 +84,55 @@ const Login = (props) => {
   };
 
   const collegeNameChangeHandler = (event) => {
-    dispatchCollegeName({type:'User_Input', val:event.target.value})
+    dispatchCollegeName({ type: 'User_Input', val: event.target.value })
     setFormIsValid(
       emailState.isValid && passwordState.isValid && collegeNameState.isValid
     );
   }
 
   const validateEmailHandler = () => {
-    dispatchEmail({type:'Input_Blur' })
+    dispatchEmail({ type: 'Input_Blur' })
   };
 
   const validatePasswordHandler = () => {
-    dispatchPassword({type:'Input_Blur'})
+    dispatchPassword({ type: 'Input_Blur' })
   };
 
   const validateCollegeNameHandler = () => {
-    dispatchCollegeName({type:'Input_Blur'})
+    dispatchCollegeName({ type: 'Input_Blur' })
   }
+  const authCtx = useContext(AuthContext);
   const submitHandler = (event) => {
     event.preventDefault();
-    props.onLogin(emailState.value, passwordState.value, collegeNameState.value);
+    authCtx.onLogin(emailState.value, passwordState.value, collegeNameState.value);
   };
-
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
-        <div
-          className={`${classes.control} ${emailState.isValid === false ? classes.invalid : ''
-            }`}
-        >
-          <label htmlFor="email">E-Mail</label>
-          <input
-            type="email"
-            id="email"
-            value={emailState.value}
-            onChange={emailChangeHandler}
-            onBlur={validateEmailHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${passwordState.isValid === false ? classes.invalid : ''
-            }`}
-        >
-          <label htmlFor="password">Password</label>
-          <input
-            type="password"
-            id="password"
-            value={passwordState.value}
-            onChange={passwordChangeHandler}
-            onBlur={validatePasswordHandler}
-          />
-        </div>
-        <div
-          className={`${classes.control} ${collegeNameState.isValid === false ? classes.invalid : ''
-            }`}
-        >
-          <label htmlFor="collegename">College Name</label>
-          <input
-            type="collegename"
-            id="collegename"
-            value={collegeNameState.value}
-            onChange={collegeNameChangeHandler}
-            onBlur={validateCollegeNameHandler}
-          />
-        </div>
+        <Input
+          id='email'
+          type="email"
+          label="E-Mail"
+          isValid={emailState.isValid} value={emailState.value}
+          onChange={emailChangeHandler}
+          onBlur={validateEmailHandler}
+        ></Input>
+        <Input
+          id='password'
+          type="password"
+          label="Password"
+          isValid={passwordState.isValid} value={passwordState.value}
+          onChange={passwordChangeHandler}
+          onBlur={validatePasswordHandler}
+        ></Input>
+        <Input
+          id='collegename'
+          type="collegename"
+          label="College Name"
+          isValid={collegeNameState.isValid} value={collegeNameState.value}
+          onChange={collegeNameChangeHandler}
+          onBlur={validateCollegeNameHandler}
+        ></Input>
         <div className={classes.actions}>
           <Button type="submit" className={classes.btn} disabled={!formIsValid}>
             Login
--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.css';
+import { AuthContextProvider } from './Store/auth-context';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<AuthContextProvider>
+    <App />
+</AuthContextProvider>);
